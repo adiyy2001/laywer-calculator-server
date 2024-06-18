46016968d229227d33202f13a62fc1a5e149557d
adiyy2001 <adiy@onet.pl>
2024-06-18 17:19:49 +0300

    update db

--- api/utils/puppeteerUtils.ts
+++ api/utils/puppeteerUtils.ts
@@ -29,8 +29,8 @@ export const fetchWiborRates = async (startDateString: string): Promise<Rates[]>
       defaultViewport: null
     });
 
-    const latestRate = await getLatestRate();
-    let startDate = latestRate ? new Date(latestRate.date) : new Date(startDateString);
+    // const latestRate = await getLatestRate();
+    let startDate =  new Date(startDateString);
 
     // Sprawdź, czy data początkowa to piątek
     if (startDate.getDay() === 5) {
