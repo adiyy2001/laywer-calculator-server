71833015ca8a92c0c9586894dfe714a78aaf2237
adiy2001 <adiy@onet.pl>
2024-08-02 16:37:33 +0200

    Remove node_modules, dist, .env, and credentials.json from repository

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WIBOR_URL = exports.PORT = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.PORT = process.env.PORT || 3001;
-exports.WIBOR_URL = process.env.WIBOR_URL || 'https://www.bankier.pl/mieszkaniowe/stopy-procentowe/wibor';
