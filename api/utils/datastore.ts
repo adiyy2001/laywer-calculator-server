9113c04b252b03dbe2b96b1b2e233a5fea4e4a83
adiy2001 <adiy@onet.pl>
2024-08-02 15:33:04 +0200

    udpate

--- api/utils/datastore.ts
+++ api/utils/datastore.ts
@@ -1,13 +1,26 @@
 import { Datastore } from "@google-cloud/datastore";
-import path from "path";
 import { config } from "dotenv";
 import fs from "fs";
+import path from "path";
+
+// Załaduj zmienne środowiskowe
 config();
 
+// Ścieżka do katalogu z poświadczeniami
 const dir = path.resolve(__dirname, "../../");
 console.log(`Resolved dir: ${dir}`);
 
-const serviceAccountPath = path.join(dir, "credentials.json");
+const credentialsJson = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
+if (!credentialsJson) {
+  throw new Error(
+    "GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable is not set."
+  );
+}
+
+// Zapisz poświadczenia do tymczasowego pliku (tylko w razie potrzeby)
+const serviceAccountPath = path.join(dir, "temp-credentials.json");
+fs.writeFileSync(serviceAccountPath, credentialsJson);
+
 console.log(`Service Account Path: ${serviceAccountPath}`);
 
 if (!fs.existsSync(serviceAccountPath)) {
@@ -18,5 +31,8 @@ const datastore = new Datastore({
   projectId: "lawyer-calculator", // Zamień na ID swojego projektu
   keyFilename: serviceAccountPath,
 });
+
+// Po zainicjalizowaniu Datastore, usuń tymczasowy plik z poświadczeniami
+fs.unlinkSync(serviceAccountPath);
 
 export { datastore };
