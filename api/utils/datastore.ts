023050d11258b570e6dd406113e5c8ff84464a42
adiy2001 <adiy@onet.pl>
2024-08-02 18:34:39 +0200

    update

--- api/utils/datastore.ts
+++ api/utils/datastore.ts
@@ -1,14 +1,8 @@
 import { Datastore } from "@google-cloud/datastore";
 import { config } from "dotenv";
-import fs from "fs";
-import path from "path";
 
 // Załaduj zmienne środowiskowe
 config();
-
-// Ścieżka do katalogu z poświadczeniami
-const dir = path.resolve(__dirname, "../../");
-console.log(`Resolved dir: ${dir}`);
 
 // Produkcja: użyj zmiennej środowiskowej
 const credentialsJson = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
@@ -17,9 +11,22 @@ if (!credentialsJson) {
     "GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable is not set."
   );
 }
+
+let credentials;
+try {
+  credentials = JSON.parse(credentialsJson);
+} catch (error) {
+  throw new Error(
+    "Invalid JSON in GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable."
+  );
+}
+
 const datastore = new Datastore({
-  projectId: "lawyer-calculator",
-  keyFilename: credentialsJson,
+  projectId: credentials.project_id,
+  credentials: {
+    client_email: credentials.client_email,
+    private_key: credentials.private_key,
+  },
 });
 
 export { datastore };
